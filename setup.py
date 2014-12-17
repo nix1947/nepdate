851e149ea8e6bb82694aacc37f128a414d9b0b8b
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-17 15:32:48 +0545

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='nepdate',
+    version='0.9',
+    author='Shritesh Bhattarai',
+    packages=['nepdate'],
+    url='http://github.com/xtranophilist/nepdate/',
+    license='BSD License',
+    description='A python library to convert dates from B.S. to A.D.',
+    include_package_data=True,
+    zip_safe=False,
+    keywords='nepdate, bikram, sambat, nepal, calendar',
+)
